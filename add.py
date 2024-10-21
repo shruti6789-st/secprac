3dd3340abf0c79f8667c6bba5889f2760fef2eca
shruti6789-st <shruti.17181@sakec.ac.in>
2024-10-21 11:35:41 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,12 @@
+def main():
+    first = 10
+    second = 20
+ 
+    # Add the two numbers
+    sum_ = first + second
+ 
+    # Print the result
+    print(f"{first} + {second} = {sum_}")
+ 
+if __name__ == "__main__":
+    main()
