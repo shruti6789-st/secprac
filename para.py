d0d72e8ee16fb9c6ca1617799793ef83085a19b1
shruti6789-st <shruti.17181@sakec.ac.in>
2024-10-21 11:35:00 +0530

    Create para.py

---
+++ para.py
@@ -0,0 +1,11 @@
+import sys
+
+# Get the parameters passed from Jenkins
+num1 = int(sys.argv[1])
+num2 = int(sys.argv[2])
+
+# Perform addition
+result = num1 + num2
+
+# Print the result
+print(f"The sum of {num1} and {num2} is {result}")
